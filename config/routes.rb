f29f53e9cee20d90cdfad0f2f05d729de1736b94
Yuli <colinxt@gmail.com>
2013-03-13 14:24:24 +0800

    Finish deploy to Heroku w/ user

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,4 @@ Myblog::Application.routes.draw do
   get "static_pages/about"
   match '/about', to: 'static_pages#about'
 
-  get "users/new"
-  match '/signup', to: 'users#new'
 end
--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -2,11 +2,4 @@ require 'spec_helper'
 
 describe "UserPages" do
 
-  subject { page }
-
-  describe "signup page" do
-    before { visit signup_path }
-
-    it { should have_selector('h1',    text: 'Sign up') }
-  end
 end
