44b5c08deebe5483cddbffcb96be3a4469bcdb4a
Yuli <colinxt@gmail.com>
2013-03-13 16:52:10 +0800

    Add Sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Myblog::Application.routes.draw do
+  resources :sessions, only: [:new, :create, :destroy]
+
   root to: 'static_pages#about'
 
   get "static_pages/about"
   match '/about', to: 'static_pages#about'
 
+  match '/jinqu', to: 'sessions#new'
+  match '/chulai', to: 'sessions#destroy', via: :delete
 end
