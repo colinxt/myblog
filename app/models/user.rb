ec23c1ab9e550226e9c4badaec6de65be4739ef5
Yuli <colinxt@gmail.com>
2013-03-13 12:11:50 +0800

    Add basic validations to name attr

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   attr_accessible :name
+
+  VALID_NAME_REGEX = /[a-zA-Z" ".]+/
+  validates :name, presence: true, length: { maximum: 15 }, 
+            format: { with: VALID_NAME_REGEX }, uniqueness: true
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,52 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @user = User.new(name: "Example User")}
+
+  subject { @user }
+
+  it { should respond_to(:name) }
+
+  it { should be_valid }
+
+  describe "when name is not present" do
+    before { @user.name = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when name is too long" do
+    before { @user.name = "a" * 16 }
+    it { should_not be_valid }
+  end
+
+  describe "when name format is valid" do
+    it "should be valid" do
+      names = ['A B', 'A.B', 'A. B', 'A B.', 'A', 'B']
+      names.each do |valid_name|
+        @user.name = valid_name
+        @user.should be_valid
+      end
+    end
+  end
+
+  describe "when name format is invalid" do
+    it "should not be valid" do
+      names = %w[\\ ~ ! @ # $ % ^ & * ( ) _ - + = : ; ' < > ? / { } [ ] | ]
+      names.each do |invalid_name|
+        @user.name = invalid_name
+        @user.should_not be_valid
+      end
+    end
+  end
+
+  describe "when name is already taken" do
+    before do
+      user_with_same_name = @user.dup
+      user_with_same_name.save
+    end
+
+    it { should_not be_valid }
+  end
+
+
 end
