f444257bb1da35183e8a5c9c7fd43813b4c34f27
Yuli <colinxt@gmail.com>
2013-03-13 22:53:14 +0800

    Finish sign-in sign-out

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,16 @@ class User < ActiveRecord::Base
   attr_accessible :name, :password
   has_secure_password
 
+  before_save :create_remember_token
+
   VALID_NAME_REGEX = /[a-zA-Z" ".]+/
   validates :name, presence: true, length: { maximum: 15 }, 
             format: { with: VALID_NAME_REGEX }, uniqueness: true
   validates :password, presence: true
 
+  private
+
+    def create_remember_token 
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
