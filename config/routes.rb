6f9c546e56baf15ad105c91c37f352fa58b63b10
Yuli <colinxt@gmail.com>
2013-03-13 14:07:11 +0800

    Misc.

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,8 @@ Myblog::Application.routes.draw do
   root to: 'static_pages#about'
 
   get "static_pages/about"
-  # match '/about', to: 'static_pages#about'
+  match '/about', to: 'static_pages#about'
 
   get "users/new"
   match '/signup', to: 'users#new'
-
-  
 end
