00bcd1415568a99d5a5ced2ce3159bbe95ccb9c6
Yuli <colinxt@gmail.com>
2013-03-15 10:07:14 +0800

    Add Post model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130313105007) do
+ActiveRecord::Schema.define(:version => 20130315020040) do
+
+  create_table "posts", :force => true do |t|
+    t.string   "title"
+    t.string   "content"
+    t.boolean  "public"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "posts", ["created_at"], :name => "index_posts_on_created_at"
 
   create_table "users", :force => true do |t|
     t.string   "name"
