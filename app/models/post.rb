10090fc711b67125b69e8c0fa027ba85911e4b2c
Yuli <colinxt@gmail.com>
2013-03-15 10:26:24 +0800

    Add tests for presence of posts attr

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ActiveRecord::Base
-  attr_accessible :content, :title
+  attr_accessible :content, :title, :public
+  validates :title, :content, :public, presence: true
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,29 @@
 require 'spec_helper'
 
 describe Post do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @post = Post.new(title: "Im title", content: "testtesttest", public: true)
+  end
+
+  subject { @post }
+
+  it { should respond_to(:title) }
+  it { should respond_to(:content) }
+  it { should respond_to(:public) }
+
+  describe "when title is not present" do
+    before { @post.title = nil }
+    it { should_not be_valid }
+  end
+
+  describe "when content is not present" do
+    before { @post.content = nil }
+    it { should_not be_valid }
+  end
+
+  describe "when public is not present" do
+    before { @post.public = nil }
+    it { should_not be_valid }
+  end
+
 end
