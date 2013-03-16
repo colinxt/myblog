b907667650eda21d568ac610b2f1acff7e1df0d3
Yuli <colinxt@gmail.com>
2013-03-16 22:45:28 +0800

    Small fix for post_tests

--- spec/features/post_pages_spec.rb
+++ spec/features/post_pages_spec.rb
@@ -8,7 +8,7 @@ describe "PostPages" do
   before { sign_in user}
 
   describe "post creation" do
-    before { visit root_path }
+    before { visit new_post_path }
 
     describe "with invalid information" do
       it "should not create a post" do
@@ -17,10 +17,29 @@ describe "PostPages" do
     end
 
     describe "with valid information" do
-      before { fill_in 'post_content', with: "testesteste"}
+      before do
+        fill_in "post_title", with: "testesteste"
+        fill_in "post_content", with: "testesteste"
+        select "Public", from: "post_public"
+      end
       it "should create a post" do
         expect { click_button "Post" }.to change(Post, :count).by(1)
       end
     end
   end
+
+  # describe "post edit" do
+  #   let!(@a_post) { Post.create(title: "fdf", content: "qqq", public: "true") }
+  #   visit edit_post_path(@a_post)
+
+  #   it { should have_selector('input', value: "fdf") }
+  #   it { should have_selector('textarea', test: "qqq") }
+  #   it { should have_selector('option', selected: "selected", text: "Public") }
+  # end
+
+  # describe "post deletion" do
+  #   let!(:post) { Post.create(title: "fdf", content: "qqq", public: "true") }
+  #   visit post_path(post)
+  #   expect { click_link "Delete Post" }.to change(Post, :count).by(-1)
+  # end
 end
