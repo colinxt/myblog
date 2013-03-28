bae740f3de8f913497b69e93c7c42a2e6c35b731
Yuli <colinxt@gmail.com>
2013-03-28 20:46:57 +0800

    Finish lang view

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
-  attr_accessible :content, :title, :public
-  validates :title, :content, presence: true
-  default_scope order: 'posts.created_at DESC'
+  attr_accessible :content, :title, :public, :lang
+  validates :title, :content, :lang, presence: true
+  validates :lang, inclusion: %w(eng chn)
+  default_scope order: 'posts.updated_at DESC'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130315040110) do
+ActiveRecord::Schema.define(:version => 20130328021252) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20130315040110) do
     t.boolean  "public"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "lang"
   end
 
   add_index "posts", ["created_at"], :name => "index_posts_on_created_at"
