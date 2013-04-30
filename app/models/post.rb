69fb0139c2611ec488253fb15f5bdcc043f0f09f
Yuli <colinxt@gmail.com>
2013-04-30 23:54:40 +0800

    Add tags model and relationship, show it on pages

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ActiveRecord::Base
   validates :title, :content, :lang, presence: true
   validates :lang, inclusion: %w(eng chn)
   default_scope order: 'posts.updated_at DESC'
+  has_and_belongs_to_many :tags
 end
